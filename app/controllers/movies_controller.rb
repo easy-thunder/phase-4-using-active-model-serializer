9decc3d7eb9d1ba85b30fbd563aea23dd27692b6
Jacob David Diehl <jakediehl17@gmail.com>
2023-02-24 11:34:44 -0700

    commit tree 1a4f7661097b82e1f82e1f10de4ef7acda2bf06d

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,23 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
+  def summary 
+    movie = Movie.find_by!(id: params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+  
+  def summaries 
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+
   private
 
   def render_not_found_response
     render json: { error: "Movie not found" }, status: :not_found
   end
+
+
+
+
 end
